2961acd425e6db6aa6e17b55b970c2551b61a37b
Glitch (ariana-webpage) <none>
2020-12-18 14:35:28 +0000

    🕶🐗 Updated with Glitch

--- script.js
+++ script.js
@@ -40,3 +40,12 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 3000 ); // Change image every 4 seconds
 }
+
+
+function mOver(obj) {
+  obj.innerHTML = "Yes i have!"
+}
+
+function mOut(obj) {
+  obj.innerHTML = "Have you understood everything?"
+}
